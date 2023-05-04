d0941a736d614b19e69de492afaeedcde97c6b32
Rocco Caccioppoli <rocco.caccioppoli@cmcc.it>
2023-05-04 10:16:27 +0200

    first commit + adding lib as separate repo

---
+++ __init__.py
@@ -0,0 +1,7 @@
+"""This package provides functions for optimizing and saving the results of machine learning models.
+
+The `classification` module provides the following functions to train classification models:
+- optimizer: Optimizes a classification model using a specified search algorithm.
+
+Functions for other types of machine learning algorithms will also be added in the future
+"""
